4e30c4dca243911db40fe87d81d406052850e40a
robibey <robert.ibey603@gmail.com>
2022-12-26 03:03:20 -0500

    implemented multiprocessing

--- sort.py
+++ sort.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import datetime
 import cv2
+from multiprocessing import Pool
     
 def main():
     while True:
@@ -12,31 +13,39 @@ def main():
         break
     print()
     print('Detecting corrupted or invalid images.')
-    good_files = find_bad_files(source_path)
-    print()
-
-def find_bad_files(source_path: str):
-    counter = 0
-    bad_counter = 0
-    bad_files = []
-    good_files = []
+    images = []
     for root, _, files in os.walk(source_path):
         for file in files:
-            counter += 1
-            try:
-                cv2.imread(os.path.join(root, file))
-                good_files.append(os.path.join(root, file))
-            except:
-                bad_counter += 1
-                bad_files.append(os.path.join(root, file))
+            images.append(os.path.join(root, file))
+
+    with Pool() as pool:
+        multiprocess_pool = pool.map(multi_cv2_bad_files, images)
+    good_list = [i for output in multiprocess_pool for i in output[0]]
+    bad_list = [j for output in multiprocess_pool for j in output[1]]
+
+    find_bad_files(good_list, bad_list)
+
+def multi_cv2_bad_files(file):
+    good_files = []
+    bad_files = []
+    if cv2.imread(file) is not None:
+        good_files.append(file)
+    else:
+        bad_files.append(file)
+    return good_files, bad_files
+
+def find_bad_files(good_files: list, bad_files: list):
+    counter = len(good_files) + len(bad_files)
+    bad_counter = len(bad_files)
     if bad_files:
-        print(f'Found {bad_counter} bad files. File paths printed below, please review.')
+        print(f'Found {bad_counter} bad file(s). File paths printed below, please review.')
         print()
         print(*bad_files, sep=', ')
         print()
 
         while True:
-            user_choice = input('You have the choice to move (mov) the unreadable files to a different directory, delete (del) the unreadable files, inputting anything else or just pressing enter will ignore the unreadable files.')
+            user_choice = input('You have the choice to move (mov) the unreadable files to a different directory, delete (del) the unreadable files, inputting anything else or just pressing enter will ignore the unreadable files. ')
+            print()
 
             if user_choice == 'mov':
                 move_path = input('Please input a directory to move the unreadable files to: ')
@@ -49,7 +58,7 @@ def find_bad_files(source_path: str):
                     continue
 
             elif user_choice == 'del':
-                confirmation = input('Are you sure you want to delete the unreadable files? These files may be unrecoverable. y/n')
+                confirmation = input('Are you sure you want to delete the unreadable files? These files may be unrecoverable. y/n ')
                 if confirmation == 'y':
                     for file in bad_files:
                         os.remove(file)
@@ -58,7 +67,7 @@ def find_bad_files(source_path: str):
                     continue
 
             else:
-                confirm = input('Are you sure you want to move the unreadable files along with the readable files? y/n')
+                confirm = input('Are you sure you want to move the unreadable files along with the readable files? y/n ')
                 if confirm == 'y':
                     print('Ignoring the unreadable files. They will be sorted with the rest of the files.')
                     good_files += bad_files
@@ -68,7 +77,8 @@ def find_bad_files(source_path: str):
         print()
     else:
         print('No bad files found. Continuing.')
-    print(f'Looked through {counter} files. {bad_counter} unreadable files and {counter-bad_counter} readable files.')
+
+    print(f'Looked through {counter} files. {bad_counter} unreadable file(s) and {counter-bad_counter} readable files.')
     return good_files
 
 if __name__ == '__main__':
